1e08605e9d3694d390d128ed41fe75be2e05215d
深圳骏马车服 <904317764@qq.com>
2021-07-28 23:21:09 +0800

    add JsonResponseWithData

--- frame/q/response.go
+++ frame/q/response.go
@@ -16,7 +16,7 @@ func ResponseWithMeta(r *ghttp.Request, err error, data interface{}, total int64
 		qservice.ReqContext.SetError(r.Context(), err)
 		JsonExit(r, 1, err.Error())
 	} else {
-		JsonExit(r, 0, "ok", dataToLowerCamelMap(data), total)
+		JsonExit(r, 0, "ok", data, total)
 	}
 }
 
@@ -25,7 +25,7 @@ func ResponseWithData(r *ghttp.Request, err error, data interface{}) {
 		qservice.ReqContext.SetError(r.Context(), err)
 		JsonExit(r, 1, err.Error())
 	} else {
-		JsonExit(r, 0, "ok", dataToLowerCamelMap(data))
+		JsonExit(r, 0, "ok", data)
 	}
 }
 
@@ -101,6 +101,11 @@ func lowerCamelMapArr(ms []interface{}) {
 
 // 数据返回通用JSON数据结构
 type JsonResponse struct {
+	Code    int    `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
+	Message string `json:"message"` // 提示信息
+}
+
+type JsonResponseWithData struct {
 	Code    int         `json:"code"`    // 错误码((0:成功, 1:失败, >1:错误码))
 	Message string      `json:"message"` // 提示信息
 	Data    interface{} `json:"data"`    // 返回数据(业务接口定义具体数据结构)
@@ -115,33 +120,37 @@ type ResMeta struct {
 // 数据返回通用JSON数据结构(带分页)
 type JsonResponseWithMeta struct {
 	Meta ResMeta `json:"meta"`
-	JsonResponse
+	JsonResponseWithData
 }
 
 // 标准返回结果数据结构封装。
 func Json(r *ghttp.Request, code int, message string, data ...interface{}) {
-	responseData := interface{}(nil)
-	if len(data) > 0 {
-		responseData = data[0]
-	}
-	jsonResponse := JsonResponse{
-		Code:    code,
-		Message: message,
-		Data:    responseData,
-	}
-
-	if len(data) > 1 {
-		r.Response.WriteJson(JsonResponseWithMeta{
-			JsonResponse: jsonResponse,
-			Meta: ResMeta{
-				Total:    data[1].(int64),
-				Current:  r.GetQueryInt64("current"),
-				PageSize: r.GetQueryInt64("pageSize"),
-			},
+	if len(data) == 0 {
+		r.Response.WriteJson(JsonResponse{
+			Code:    code,
+			Message: message,
 		})
 	} else {
-		r.Response.WriteJson(jsonResponse)
+		jsonResponseWithData := JsonResponseWithData{
+			Code:    code,
+			Message: message,
+			Data:    dataToLowerCamelMap(data[0]),
+		}
+		if len(data) == 1 {
+			r.Response.WriteJson(jsonResponseWithData)
+		}
+		if len(data) > 1 {
+			r.Response.WriteJson(JsonResponseWithMeta{
+				JsonResponseWithData: jsonResponseWithData,
+				Meta: ResMeta{
+					Total:    data[1].(int64),
+					Current:  r.GetQueryInt64("current"),
+					PageSize: r.GetQueryInt64("pageSize"),
+				},
+			})
+		}
 	}
+
 }
 
 func HttpStatus(r *ghttp.Request, err int) {
@@ -151,8 +160,8 @@ func HttpStatus(r *ghttp.Request, err int) {
 }
 
 // 返回JSON数据并退出当前HTTP执行函数。
-func JsonExit(r *ghttp.Request, err int, msg string, data ...interface{}) {
-	HttpStatus(r, err)
-	Json(r, err, msg, data...)
+func JsonExit(r *ghttp.Request, code int, msg string, data ...interface{}) {
+	HttpStatus(r, code)
+	Json(r, code, msg, data...)
 	r.Exit()
 }
