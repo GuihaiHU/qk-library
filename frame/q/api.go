dcee53b3e44a6e8de024c8c8fcf13fe7b3773c53
深圳骏马车服 <904317764@qq.com>
2021-07-30 15:40:51 +0800

    support default and ignore

--- frame/q/api.go
+++ frame/q/api.go
@@ -1,8 +1,10 @@
 package q
 
 import (
+	"errors"
 	"fmt"
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/gogf/gf/frame/g"
@@ -27,12 +29,32 @@ func AssignParamFormReq(r *ghttp.Request, param interface{}) {
 		}
 	}
 
+	// 解析标签
+	parseParamByTag(r, param)
+}
+
+func parseParamByTag(r *ghttp.Request, param interface{}) {
 	dtoType := reflect.TypeOf(param).Elem()
 	dtoValue := reflect.ValueOf(param).Elem()
 
 	for i := 0; i < dtoType.NumField(); i++ {
 		itemType := dtoType.Field(i)
 		itemValue := dtoValue.Field(i)
+
+		// 忽略 swagger:ignore 的值
+		ignoreTag := itemType.Tag.Get("swagger")
+		if ignoreTag == "ignore" && !itemValue.IsNil() {
+			err := fmt.Errorf("不允许传递%s参数", itemType.Name)
+			g.Log("exception").Error(err)
+			JsonExit(r, 400, err.Error())
+		}
+
+		// 设置默认值
+		defaultTag, isTagExisted := itemType.Tag.Lookup("default")
+		if isTagExisted {
+			defaultValue := getDefaultValue(r, itemType.Type, defaultTag)
+			itemValue.Set(reflect.ValueOf(defaultValue))
+		}
 
 		// 通过ctx标签获取
 		ctxTag := itemType.Tag.Get("ctx")
@@ -41,6 +63,11 @@ func AssignParamFormReq(r *ghttp.Request, param interface{}) {
 			ctxTagName := arr[0]
 			ctxVar := r.GetCtxVar(ctxTagName).Interface()
 			ctxVarRef := reflect.ValueOf(ctxVar)
+			if ctxVarRef.IsNil() {
+				err := fmt.Errorf("获取不到%s的值", ctxTag)
+				g.Log("exception").Error(err)
+				JsonExit(r, 500, err.Error())
+			}
 			if len(arr) == 1 {
 				itemValue.Set(ctxVarRef)
 			} else {
@@ -49,4 +76,44 @@ func AssignParamFormReq(r *ghttp.Request, param interface{}) {
 			}
 		}
 	}
+}
+
+func getDefaultValue(r *ghttp.Request, typ reflect.Type, defaultTag string) (defaultValue interface{}) {
+	switch typ.Kind() {
+	case reflect.String:
+		defaultValue = defaultTag
+	case reflect.Bool:
+		if value, err := strconv.ParseBool(defaultTag); err != nil {
+			g.Log("exception").Error(err)
+			JsonExit(r, 500, err.Error())
+		} else {
+			defaultValue = value
+		}
+	case reflect.Uint:
+		if value, err := strconv.ParseUint(defaultTag, 10, 64); err != nil {
+			g.Log("exception").Error(err)
+			JsonExit(r, 500, err.Error())
+		} else {
+			defaultValue = value
+		}
+	case reflect.Float32:
+		if value, err := strconv.ParseFloat(defaultTag, 32); err != nil {
+			g.Log("exception").Error(err)
+			JsonExit(r, 500, err.Error())
+		} else {
+			defaultValue = value
+		}
+	case reflect.Float64:
+		if value, err := strconv.ParseFloat(defaultTag, 64); err != nil {
+			g.Log("exception").Error(err)
+			JsonExit(r, 500, err.Error())
+		} else {
+			defaultValue = value
+		}
+	default:
+		err := errors.New("不支持的默认值类型" + typ.String())
+		g.Log("exception").Error(err)
+		JsonExit(r, 500, err.Error())
+	}
+	return
 }
