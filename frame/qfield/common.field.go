504c6b4ddb4e973ce92f2721604c6e458f5e5820
深圳骏马车服 <904317764@qq.com>
2021-08-03 21:28:11 +0800

    add qfield.Id and interface qfield.IdParam

--- frame/qfield/common.field.go
+++ frame/qfield/common.field.go
@@ -3,3 +3,14 @@ package qfield
 type Id struct {
 	Id *uint `where:"" example:"1" v:"required|integer#id必填|id必须为整数"`
 }
+
+func (f *Id) GetId() uint {
+	if *f.Id == 0 {
+		panic("Id 不允许为空")
+	}
+	return *f.Id
+}
+
+type IdParam interface {
+	GetId() uint
+}
